f207c47878373b5293a68308fc9604fd45de896e
DOMINATOR\Fran1 <fran11799@outlook.com>
2026-03-10 13:22:30 +0100

    fix(load): preload all models and initial textures before rendering scene

--- src/components/SceneViewport.vue
+++ src/components/SceneViewport.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
-import { TresCanvas } from '@tresjs/core'
+import { TresCanvas, useTexture } from '@tresjs/core'
+import { useGLTF } from '@tresjs/cientos'
 import { TresLeches, useControls } from '@tresjs/leches'
 import { ref } from 'vue'
 import TheExperience from './TheExperience.vue'
+import { useState } from '../composables/useState'
 
 const renderTimes = ref(0)
+const { tableMaterials } = useState()
+const initialMaterial = tableMaterials.value[0]
+
+await Promise.all([
+  useGLTF('/models/laptop.glb', { draco: true }),
+  useGLTF('/models/standing-desk.glb', { draco: true }),
+  useGLTF('/models/plant.glb', { draco: true }),
+  useGLTF('/models/headphones.glb', { draco: true }),
+  useTexture({
+    map: `${initialMaterial.path}/${initialMaterial.filename}_Color.webp`,
+    normalMap: `${initialMaterial.path}/${initialMaterial.filename}_NormalDX.webp`,
+    roughnessMap: `${initialMaterial.path}/${initialMaterial.filename}_Roughness.webp`,
+    displacementMap: `${initialMaterial.path}/${initialMaterial.filename}_Displacement.webp`,
+  }),
+])
 
 useControls('fpsgraph')
 
@@ -27,6 +44,8 @@ function onRender() {
 <template>
   <TresLeches />
   <TresCanvas render-mode="always" alpha @render="onRender">
-    <TheExperience />
+    <Suspense>
+      <TheExperience />
+    </Suspense>
   </TresCanvas>
 </template>
