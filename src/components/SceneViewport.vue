775f09345c50047575845597ed76afec1392d845
DOMINATOR\Fran1 <fran11799@outlook.com>
2026-03-10 13:24:33 +0100

    fix(load): remove blocking preload that froze suspense

--- src/components/SceneViewport.vue
+++ src/components/SceneViewport.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-import { TresCanvas, useTexture } from '@tresjs/core'
-import { useGLTF } from '@tresjs/cientos'
+import { TresCanvas } from '@tresjs/core'
 import { TresLeches, useControls } from '@tresjs/leches'
 import { ref } from 'vue'
 import TheExperience from './TheExperience.vue'
-import { useState } from '../composables/useState'
 
 const renderTimes = ref(0)
-const { tableMaterials } = useState()
-const initialMaterial = tableMaterials.value[0]
-
-await Promise.all([
-  useGLTF('/models/laptop.glb', { draco: true }),
-  useGLTF('/models/standing-desk.glb', { draco: true }),
-  useGLTF('/models/plant.glb', { draco: true }),
-  useGLTF('/models/headphones.glb', { draco: true }),
-  useTexture({
-    map: `${initialMaterial.path}/${initialMaterial.filename}_Color.webp`,
-    normalMap: `${initialMaterial.path}/${initialMaterial.filename}_NormalDX.webp`,
-    roughnessMap: `${initialMaterial.path}/${initialMaterial.filename}_Roughness.webp`,
-    displacementMap: `${initialMaterial.path}/${initialMaterial.filename}_Displacement.webp`,
-  }),
-])
 
 useControls('fpsgraph')
 
@@ -44,8 +27,6 @@ function onRender() {
 <template>
   <TresLeches />
   <TresCanvas render-mode="always" alpha @render="onRender">
-    <Suspense>
-      <TheExperience />
-    </Suspense>
+    <TheExperience />
   </TresCanvas>
 </template>
